030536ec34d6931fd0f0e0cd65a904efeb7cd90c
John Romkey <romkey@romkey.com>
2013-08-27 06:41:23 -0400

    rebuilt package.json, properly ;)
    added watch and imagemin to gruntfile
    
    added new whitelist_only option
    
    changed additional sensor code to only show whitelisted sensors
    
    bumped version number to 3.1.0

--- gruntfile.js
+++ gruntfile.js
@@ -9,7 +9,7 @@ module.exports = function( grunt ) {
     grunt.loadNpmTasks( "grunt-contrib-compress" );
     grunt.loadNpmTasks( "grunt-contrib-copy" );
     grunt.loadNpmTasks( "grunt-contrib-clean" );
-    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks( 'grunt-shell');
 
     grunt.initConfig( {
 	jshint: {
@@ -19,12 +19,15 @@ module.exports = function( grunt ) {
 		smarttabs: true
 	    }
 	},
+
 	csslint: {
 	    files: [ 'lib/css/options.css' ]
 	},
+
 	qunit: {
 	    files: ['test/**/*.html']
 	},
+
 	compress: {
 	    zip: {
 		options: {
@@ -35,6 +38,7 @@ module.exports = function( grunt ) {
 		]
 	    }
 	},
+
 	copy: {
 	    main: {
 		files: [
@@ -60,10 +64,43 @@ module.exports = function( grunt ) {
 		]
 	    }
 	},
+
+        imagemin: {
+            dist: {
+		options: {
+		    optimizationLevel: 7
+		},
+                files: [ {
+                    expand: true,
+                    cwd: 'lib/img',
+                    src: '{,*/}*.{png,jpg,jpeg}',
+                    dest: 'lib/lementor.crx/images'
+                } ]
+            }
+        },
+
+	watch: {
+	    scriptsandcss: {
+		files: [ 'lib/js/**/*.js', 'lib/css/*.css' ],
+		tasks: [ 'copy' ],
+		options: {
+		    nospawn: true
+		}
+	    },
+	    images: {
+		files: [ 'lib/img/**/*.jpg', 'lib/img/**/*.png', 'lib/img/**/*.gif' ],
+		tasks: [ 'imagemin' ],
+		options: {
+		    nospawn: true
+		}
+	    }
+	},
+
 	clean: [ 'lib/aqehelper.crx/js', 'lib/aqehelper.crx/css', 'lib/aqehelper.crx/images',
 		 'lib/aqehelper.safariextension/js', 'lib/aqehelper.safariextension/css', 'lib/aqehelper.safariextension/img',
 		 'lib/aqehelper.xpi/data/js', 'lib/aqehelper.xpi/data/css', 'lib/aqehelper.xpi/data/img',
 		 'dist' ],
+
 	shell: {
 	    firefox: {
 		command: 'cfx run',
